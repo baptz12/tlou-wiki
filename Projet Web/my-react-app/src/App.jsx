10490643e110fac008bc821cc4be28a156b88257
Baptiste ZETLER <baptiste.zetler@epitech.eu>
2024-06-04 16:52:03 +0200

    Feat : new cards and some fixes

--- Projet Web/my-react-app/src/App.jsx
+++ Projet Web/my-react-app/src/App.jsx
@@ -1,14 +1,30 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
-import Food from './Food.jsx'
 import Card from './Card.jsx'
+import runnersPic from './assets/runners.jpg';
+import stalkersPic from './assets/stalkers.jpg';
+import clickersPic from './assets/clickers.jpg';
 
 function App() {
 
   return (
     <>
     <Header></Header>
-    <Card></Card>
+    <Card 
+      image={runnersPic}
+      title="Les coureurs" 
+      text="Les coureurs sont les premiers et les plus faibles des infectés qui apparaissent dans The Last of Us." 
+    />
+    <Card 
+      image={stalkersPic}
+      title="Les rôdeurs" 
+      text="Les rôdeurs sont le deuxième stade des infectés dans The Last of Us. Il leur faut entre deux semaines et un an pour atteindre ce stade d'infection." 
+    />
+      <Card 
+      image={clickersPic}
+      title="Les claqueurs" 
+      text="Les cliqueurs constituent le troisième stade de l'infection dans The Last of Us, ils ont subi une exposition prolongée au champignon et possèdent désormais une force qui dépasse largement celle d'un humain moyen." 
+    />
     <Footer></Footer>
     </>
   );
--- Projet Web/my-react-app/src/Card.jsx
+++ Projet Web/my-react-app/src/Card.jsx
@@ -1,13 +1,12 @@
-import profilePic from './assets/runners.jpg'
 
-function Card(){
-    return(
+function Card({ image, title, text }) {
+    return (
         <div className="card">
-            <img className="card-image" src= {profilePic} alt="profile picture"></img>
-            <h2 className="card-title">Les coureurs</h2>
-            <p className="card-text">Les coureurs sont les premiers et les plus faibles des infectés qui apparaissent dans The Last of Us.</p>
+            <img className="card-image" src={image} alt="profile picture" />
+            <h2 className="card-title">{title}</h2>
+            <p className="card-text">{text}</p>
         </div>
     );
 }
 
-export default Card
+export default Card;
