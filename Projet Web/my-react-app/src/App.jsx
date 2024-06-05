928f19415f3df9fe817cfe4fb96d70377aa303dd
Baptiste ZETLER <baptiste.zetler@epitech.eu>
2024-06-05 12:32:01 +0200

    Fix : 404 when refresh (hope it works)

--- Projet Web/my-react-app/src/App.jsx
+++ Projet Web/my-react-app/src/App.jsx
@@ -1,18 +1,18 @@
-import Header from './Header.jsx'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Footer from './Footer.jsx'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
 import Accueil from './home/Home.jsx';
 import Ennemis from './enemis/Enemies.jsx';
 import Contact from './contact/Contact.jsx';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/tlou-wiki">
       <Header />
       <Routes>
         <Route path="/" element={<Accueil />} />
         <Route path="/ennemis" element={<Ennemis />} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </Router>
