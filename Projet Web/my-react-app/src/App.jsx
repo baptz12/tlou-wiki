fbd98ba1a6099e23eb15a616c99ac75545923b3f
Baptiste ZETLER <baptiste.zetler@epitech.eu>
2024-06-05 14:25:11 +0200

    Fix : hope fix 404 V4

--- Projet Web/my-react-app/src/App.jsx
+++ Projet Web/my-react-app/src/App.jsx
@@ -1,21 +1,21 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
+import { Route, Routes } from 'react-router-dom';
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
 import Accueil from './home/Home.jsx';
 import Ennemis from './enemis/Enemies.jsx';
-import Contact from './contact/Contact.jsx'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Contact from './contact/Contact.jsx';
 
 function App() {
   return (
-    <Router>
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<Accueil />} />
         <Route path="/ennemis" element={<Ennemis />} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
-    </Router>
+    </>
   );
 }
 
--- Projet Web/my-react-app/vite.config.js
+++ Projet Web/my-react-app/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
   base: '/tlou-wiki/',
-}));
+});
