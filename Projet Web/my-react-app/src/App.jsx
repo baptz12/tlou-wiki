58de97f514a0ed307b5c377edb5956675f7cc7f4
Baptiste ZETLER <baptiste.zetler@epitech.eu>
2024-06-05 12:34:55 +0200

    Fix : stays on right page when refresh

--- Projet Web/my-react-app/src/App.jsx
+++ Projet Web/my-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import Accueil from './home/Home.jsx';
@@ -10,12 +10,21 @@ function App() {
     <Router basename="/tlou-wiki">
       <Header />
       <Routes>
-        <Route path="/" element={<Accueil />} />
-        <Route path="/ennemis" element={<Ennemis />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<AppRouter />} />
       </Routes>
       <Footer />
     </Router>
+  );
+}
+
+function AppRouter() {
+  const { page } = useParams();
+  return (
+    <Routes>
+      <Route path="/" element={<Accueil />} />
+      <Route path="/ennemis" element={<Ennemis />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
   );
 }
 
