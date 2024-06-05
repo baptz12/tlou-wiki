6afa354c4bd51e07dead520814e8be1d8d0e4b6d
Baptiste ZETLER <baptiste.zetler@epitech.eu>
2024-06-05 13:39:42 +0200

    Fix : normaly fix refresh

--- Projet Web/my-react-app/src/App.jsx
+++ Projet Web/my-react-app/src/App.jsx
@@ -1,30 +1,21 @@
-import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
-import Header from './Header.jsx';
-import Footer from './Footer.jsx';
+import Header from './Header.jsx'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Footer from './Footer.jsx'
 import Accueil from './home/Home.jsx';
 import Ennemis from './enemis/Enemies.jsx';
 import Contact from './contact/Contact.jsx';
 
 function App() {
   return (
-    <Router basename="/tlou-wiki">
+    <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<AppRouter />} />
+        <Route path="/" element={<Accueil />} />
+        <Route path="/ennemis" element={<Ennemis />} />
+        <Route path="/contact" element={<Contact/>} />
       </Routes>
       <Footer />
     </Router>
-  );
-}
-
-function AppRouter() {
-  const { page } = useParams();
-  return (
-    <Routes>
-      <Route path="/" element={<Accueil />} />
-      <Route path="/ennemis" element={<Ennemis />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
   );
 }
 
